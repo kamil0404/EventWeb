72558b3da71a6d12f944e48dddc500de1535cb9c
Kamil Maltabarov <84449358+kamil0404@users.noreply.github.com>
2023-04-05 16:08:52 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,408 @@
+const countriesList = [
+	"Afghanistan",
+	"Albania",
+	"Algeria",
+	"American Samoa",
+	"Andorra",
+	"Angola",
+	"Anguilla",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Aruba",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas (the)",
+	"Bahrain",
+	"Bangladesh",
+	"Barbados",
+	"Belarus",
+	"Belgium",
+	"Belize",
+	"Benin",
+	"Bermuda",
+	"Bhutan",
+	"Bolivia (Plurinational State of)",
+	"Bonaire, Sint Eustatius and Saba",
+	"Bosnia and Herzegovina",
+	"Botswana",
+	"Bouvet Island",
+	"Brazil",
+	"British Indian Ocean Territory (the)",
+	"Brunei Darussalam",
+	"Bulgaria",
+	"Burkina Faso",
+	"Burundi",
+	"Cabo Verde",
+	"Cambodia",
+	"Cameroon",
+	"Canada",
+	"Cayman Islands (the)",
+	"Central African Republic (the)",
+	"Chad",
+	"Chile",
+	"China",
+	"Christmas Island",
+	"Cocos (Keeling) Islands (the)",
+	"Colombia",
+	"Comoros (the)",
+	"Congo (the Democratic Republic of the)",
+	"Congo (the)",
+	"Cook Islands (the)",
+	"Costa Rica",
+	"Croatia",
+	"Cuba",
+	"Curaçao",
+	"Cyprus",
+	"Czechia",
+	"Côte d'Ivoire",
+	"Denmark",
+	"Djibouti",
+	"Dominica",
+	"Dominican Republic (the)",
+	"Ecuador",
+	"Egypt",
+	"El Salvador",
+	"Equatorial Guinea",
+	"Eritrea",
+	"Estonia",
+	"Eswatini",
+	"Ethiopia",
+	"Falkland Islands (the) [Malvinas]",
+	"Faroe Islands (the)",
+	"Fiji",
+	"Finland",
+	"France",
+	"French Guiana",
+	"French Polynesia",
+	"French Southern Territories (the)",
+	"Gabon",
+	"Gambia (the)",
+	"Georgia",
+	"Germany",
+	"Ghana",
+	"Gibraltar",
+	"Greece",
+	"Greenland",
+	"Grenada",
+	"Guadeloupe",
+	"Guam",
+	"Guatemala",
+	"Guernsey",
+	"Guinea",
+	"Guinea-Bissau",
+	"Guyana",
+	"Haiti",
+	"Heard Island and McDonald Islands",
+	"Holy See (the)",
+	"Honduras",
+	"Hong Kong",
+	"Hungary",
+	"Iceland",
+	"India",
+	"Indonesia",
+	"Iran (Islamic Republic of)",
+	"Iraq",
+	"Ireland",
+	"Isle of Man",
+	"Israel",
+	"Italy",
+	"Jamaica",
+	"Japan",
+	"Jersey",
+	"Jordan",
+	"Kazakhstan",
+	"Kenya",
+	"Kiribati",
+	"Korea (the Democratic People's Republic of)",
+	"Korea (the Republic of)",
+	"Kuwait",
+	"Kyrgyzstan",
+	"Lao People's Democratic Republic (the)",
+	"Latvia",
+	"Lebanon",
+	"Lesotho",
+	"Liberia",
+	"Libya",
+	"Liechtenstein",
+	"Lithuania",
+	"Luxembourg",
+	"Macao",
+	"Madagascar",
+	"Malawi",
+	"Malaysia",
+	"Maldives",
+	"Mali",
+	"Malta",
+	"Marshall Islands (the)",
+	"Martinique",
+	"Mauritania",
+	"Mauritius",
+	"Mayotte",
+	"Mexico",
+	"Micronesia (Federated States of)",
+	"Moldova (the Republic of)",
+	"Monaco",
+	"Mongolia",
+	"Montenegro",
+	"Montserrat",
+	"Morocco",
+	"Mozambique",
+	"Myanmar",
+	"Namibia",
+	"Nauru",
+	"Nepal",
+	"Netherlands (the)",
+	"New Caledonia",
+	"New Zealand",
+	"Nicaragua",
+	"Niger (the)",
+	"Nigeria",
+	"Niue",
+	"Norfolk Island",
+	"Northern Mariana Islands (the)",
+	"Norway",
+	"Oman",
+	"Pakistan",
+	"Palau",
+	"Palestine, State of",
+	"Panama",
+	"Papua New Guinea",
+	"Paraguay",
+	"Peru",
+	"Philippines (the)",
+	"Pitcairn",
+	"Poland",
+	"Portugal",
+	"Puerto Rico",
+	"Qatar",
+	"Republic of North Macedonia",
+	"Romania",
+	"Russian Federation (the)",
+	"Rwanda",
+	"Réunion",
+	"Saint Barthélemy",
+	"Saint Helena, Ascension and Tristan da Cunha",
+	"Saint Kitts and Nevis",
+	"Saint Lucia",
+	"Saint Martin (French part)",
+	"Saint Pierre and Miquelon",
+	"Saint Vincent and the Grenadines",
+	"Samoa",
+	"San Marino",
+	"Sao Tome and Principe",
+	"Saudi Arabia",
+	"Senegal",
+	"Serbia",
+	"Seychelles",
+	"Sierra Leone",
+	"Singapore",
+	"Sint Maarten (Dutch part)",
+	"Slovakia",
+	"Slovenia",
+	"Solomon Islands",
+	"Somalia",
+	"South Africa",
+	"South Georgia and the South Sandwich Islands",
+	"South Sudan",
+	"Spain",
+	"Sri Lanka",
+	"Sudan (the)",
+	"Suriname",
+	"Svalbard and Jan Mayen",
+	"Sweden",
+	"Switzerland",
+	"Syrian Arab Republic",
+	"Taiwan",
+	"Tajikistan",
+	"Tanzania, United Republic of",
+	"Thailand",
+	"Timor-Leste",
+	"Togo",
+	"Tokelau",
+	"Tonga",
+	"Trinidad and Tobago",
+	"Tunisia",
+	"Turkey",
+	"Turkmenistan",
+	"Turks and Caicos Islands (the)",
+	"Tuvalu",
+	"Uganda",
+	"Ukraine",
+	"United Arab Emirates (the)",
+	"United Kingdom of Great Britain and Northern Ireland (the)",
+	"United States Minor Outlying Islands (the)",
+	"United States of America (the)",
+	"Uruguay",
+	"Uzbekistan",
+	"Vanuatu",
+	"Venezuela (Bolivarian Republic of)",
+	"Viet Nam",
+	"Virgin Islands (British)",
+	"Virgin Islands (U.S.)",
+	"Wallis and Futuna",
+	"Western Sahara",
+	"Yemen",
+	"Zambia",
+	"Zimbabwe",
+	"Åland Islands"
+];
+const blogItems = [
+	item = {
+		imgSrc: "./img/blog/Rectangle 17.png",
+		title: 'How to become a DJ who is listened to',
+		desc: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+		autor: {
+			date: '15 apr',
+			name: 'Davy Jones'
+		}
+	},
+	item = {
+		imgSrc: "./img/blog/Rectangle 18.png",
+		title: '18 way to organize a buffet',
+		desc: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+		autor: {
+			date: '15 apr',
+			name: 'Davy Jones'
+		}
+	},
+	item = {
+		imgSrc: "./img/blog/Rectangle 19.png",
+		title: 'Why are dance halls so convenient?',
+		desc: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+		autor: {
+			date: '15 apr',
+			name: 'Davy Jones'
+		}
+	},
+	item = {
+		imgSrc: "./img/blog/Rectangle 20.png",
+		title: 'Russian football players plays on world cup tournament',
+		desc: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+		autor: {
+			date: '15 apr',
+			name: 'Davy Jones'
+		}
+	},
+	item = {
+		imgSrc: "./img/blog/Rectangle 21.png",
+		title: 'Event childre charity happen on church of san Mihkil of support of peace',
+		desc: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+		autor: {
+			date: '15 apr',
+			name: 'Davy Jones'
+		}
+	},
+	item = {
+		imgSrc: "./img/blog/Rectangle 22.png",
+		title: 'What is the importance of volunteer help',
+		desc: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+		autor: {
+			date: '15 apr',
+			name: 'Davy Jones'
+		}
+	},
+]
+const inputCountry = document.querySelector('#input-country')
+const countryList = document.querySelector('.country-list')
+const nextEvent = document.querySelector('.eventNext')
+const prevEvent = document.querySelector('.eventPrev')
+const eventWrapper = document.querySelector('.event-block_wrapper')
+const form = document.querySelector('.form_search-event')
+const inp = form.querySelectorAll('input')
+const blogEventItems = document.querySelector('.blog_items')
+const eventSearchBtn = document.querySelector('.form_search-btn')
+const loadMore = document.querySelector('.blogBtn')
+const createEvent = document.querySelector('.make-event_btn')
+let offset = 0
+
+nextEvent.addEventListener('click', () => {
+	offset += 100
+	if(offset > 200) offset = 0
+	eventWrapper.style.left = -offset + '%'
+})
+prevEvent.addEventListener('click', () => {
+	offset -= 100
+	if(offset < 0) offset = 200
+	eventWrapper.style.left = -offset + '%'
+})
+
+
+
+
+function createCountryList(countriesList, countryList) {
+	let innerElement = '';
+	countriesList.forEach(country => {
+		innerElement += `
+		<li class = "country-list_item">${country}</li>
+		`
+		countryList.innerHTML = innerElement
+	});
+}
+
+function filterList(countriesList, searchText) {
+	return countriesList.filter(x => x.toLowerCase().includes(searchText.toLowerCase()))
+}
+
+inputCountry.addEventListener('input', () => {
+	if(inputCountry !== '') {
+		countryList.style.display = 'block';
+		const filteredData = filterList(countriesList, inputCountry.value);
+		createCountryList(filteredData, countryList)
+		countryList.addEventListener('click', (e) => {
+			const choiceCountry = e.target.innerText;
+			inputCountry.value = choiceCountry;
+			countryList.style.display = 'none'
+		})
+	}
+})
+inputCountry.onfocus = function() {
+	countryList.style.display = 'none'
+}
+
+
+
+eventSearchBtn.onclick = function(e) {
+	e.preventDefault()
+	inp.forEach(item => {
+		console.log(item.value);
+		item.value = ''
+	})
+}
+
+loadMore.addEventListener('click', showBlogItems)
+let currentItem = 0
+function showBlogItems() {
+	let numShowItems = 3
+	for(i = 0; i < numShowItems; i++) {
+		if(currentItem >= 3) loadMore.style.display = 'none'
+		const html = `
+		<li class="item">
+			<img src="${blogItems[i +currentItem].imgSrc}" alt="blog-img">
+			<h3 class="item_title">${blogItems[i +currentItem].title}</h3>
+			<p class="item_desc">${blogItems[i +currentItem].desc}</p>
+			<div class="item_date">
+				<span class="date">${blogItems[i +currentItem].autor.date}</span> -
+				<span class="name-autor">${blogItems[i +currentItem].autor.name}</span>
+			</div>
+		</li>
+		`
+		blogEventItems.insertAdjacentHTML('beforeend', html)
+	}
+	currentItem =+ numShowItems
+}
+
+
+createEvent.addEventListener('click', () => {
+	const modalCreateEvent = document.querySelector('.own-event_modal-create-event');
+	modalCreateEvent.classList.add('open')
+	document.documentElement.classList.add('body-none-scroll')
+
+	const x = modalCreateEvent.querySelector('button')
+	x.addEventListener('click', () => {
+		modalCreateEvent.classList.remove('open');
+		document.documentElement.classList.remove('body-none-scroll')
+	})
+})
